677585cd707cfd7e3530c4fc01a0e303f876d1a1
FredNoonienSingh <FredBaumeister@Icloud.com>
2025-01-01 22:26:18 +0100

    rewritten baseClassGround

--- bot/Unit_Classes/baseClassGround.py
+++ bot/Unit_Classes/baseClassGround.py
@@ -27,56 +27,20 @@ class BaseClassGround:
 
     async def handle_attackers(self, units: Units, attack_target: Point2) -> None:
         grid: np.ndarray = self.pathing.ground_grid
-        for stalker in units:
-            
-            
-            # Keep out of Range, if Shields are low, removes to much supply from fights to fast
-            #if stalker.shield_percentage < MIN_SHIELD_AMOUNT \
-             #   and not self.pathing.is_position_safe(grid, stalker.position):
-              #  self.move_to_safety(stalker, grid)
-               # continue
 
-            # When enemy_units are visible
-            if self.bot.enemy_units:
-                visible_units = self.bot.enemy_units.closer_than(stalker.ground_range+2, stalker)\
-                    .filter(lambda unit: unit.type_id not in WORKER_IDS)
-                enemy_structs = self.bot.enemy_structures.closer_than(20, stalker)
+        for unit in units:
 
-                # Attack if Possible
-                if stalker.weapon_ready:
-                    if visible_units:
-                        target = self.pick_enemy_target(visible_units, stalker)
-                        stalker.attack(target)
-                    if not visible_units and enemy_structs:
-                        target = enemy_structs.closest_to(stalker)
-                        stalker.attack(target)
-                    if not visible_units and not enemy_structs:
-                        stalker.attack(
-                            self.pathing.find_path_next_point(
-                            stalker.position, attack_target, grid
-                            )
-                        )
+            # Does not throw an error, if "self.bot.enemy_units" is empty
+            enemies = self.bot.enemy_units.filter(lambda enemy: enemy.distance_to(unit) > unit.range+12)
 
-                # Move out of range if attacking is not possible
-                elif not stalker.weapon_ready and visible_units:
-                    threads = self.bot.enemy_units.filter(lambda Unit: Unit.distance_to(stalker) <= Unit.ground_range+1)
-                    if threads:
-                        if not self.pathing.is_position_safe(grid, stalker.position):
-                            self.move_to_safety(stalker, grid)
-                        else:
-                            continue
-                else:
-                    stalker.attack(
-                         self.pathing.find_path_next_point(
-                        stalker.position, attack_target, grid
-                    )
-                    )
+            if enemies:
+                attack_pos = self.pick_enemy_target(enemies, unit)
             else:
-                stalker.attack(
-                     self.pathing.find_path_next_point(
-                        stalker.position, attack_target, grid
-                    )
+                attack_pos = self.pathing.find_path_next_point(
+                    unit.position, attack_target, grid
                 )
+            unit.attack(attack_pos)
+      
 
     def move_to_safety(self, unit: Unit, grid: np.ndarray):
         """
@@ -92,7 +56,8 @@ class BaseClassGround:
     @staticmethod
     def pick_enemy_target(enemies: Units, attacker:Unit) -> Unit:
         #TODO: #35 This should not be tinkered with any further, TARGETING will take care of it
-        prio_targets = enemies.filter(lambda unit: unit.type_id in PRIO_ATTACK_TARGET)
+        prio_targets = enemies.filter(lambda unit: unit.type_id in PRIO_ATTACK_TARGET\
+            and not unit in ATTACK_TARGET_IGNORE)
         if prio_targets:
             return prio_targets.closest_to(attacker)
         return min(
